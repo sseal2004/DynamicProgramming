d31ffe271fc38c58d2ce01f131f526b7ae6a24a4
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-07-26 22:17:35 +0530

    Time: 1 ms (39.38%) | Memory: 45 MB (40.89%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -9,7 +9,7 @@ class Solution {
         if(index<=1) return 0;
         if(dp[index]!=-1)return dp[index];
         int left = helper(index-1,cost,dp) + cost[index-1];
-        int right = Integer.MAX_VALUE;
+        int right = Integer.MAX_VALUE;// f(1) is left + right then right is MAX_VAL
         if(index>1)
          right = helper(index-2,cost,dp) + cost[index-2];
         return dp[index] = Math.min(left,right);
