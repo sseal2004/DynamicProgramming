6e24edff11ce10e3d15a655cc5ff052b93658a28
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-07-26 19:47:16 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (73.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,11 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int [n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<dp.length;i++){
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+       int []dp = new int[n+1];
+       Arrays.fill(dp,-1);
+       return helper(dp,n);
+    }
+    public int helper (int []dp,int n){
+        if(n==0)return 1;
+       if(n==1)return 1;
+       if(dp[n]!=-1)return dp[n];
+       int left = helper(dp,n-1);
+       int right=helper(dp,n-2);
+       return dp[n]=left+right;
     }
 }
