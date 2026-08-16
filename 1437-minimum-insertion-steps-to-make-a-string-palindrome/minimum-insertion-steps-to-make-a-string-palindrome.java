6bf2760b1943591d1831817a42094de0a26a6132
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-08-16 08:30:19 +0530

    Time: 26 ms (60.24%) | Memory: 47.6 MB (29.73%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minInsertions(String s) {
+       return s.length()-longestPalindromicSubsequence(s); 
+    }
+    static int longestPalindromicSubsequence(String s){
+            StringBuilder s2 = new StringBuilder(s);
+            s2.reverse();
+            return lcs(s,s2.toString());
+    }
+    static int lcs(String s1,String s2){
+        int n= s1.length();
+        int m=s2.length();
+        int dp[][]= new int [n+1][m+1];
+        if(n!=m) return 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j] = 1+ dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
